1b8ee59adca82ee2ba3afc8ec685dae2ee3ca68b
unknown <nikitakumawat690@gmail.com>
2024-08-20 09:03:46 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
+ 
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <div className="biodata-container">
+        <div className='left'> 
+      <div className="personal-details">
+ 
+        <h2>PERSONAL DETAILS</h2>
+        <p><strong>Name      <span style={{ marginLeft: '150px' }}>:</span></strong> XXXXXXXXXXX</p>
+        <p><strong>Date of Birth  <span style={{ marginLeft: '98px' }}>:</span></strong> DD-mm-yyyy</p>
+        <p><strong>Time of Birth  <span style={{ marginLeft: '98px' }}>:</span></strong> 12:00 am</p>
+        <p><strong>Birth Place  <span style={{ marginLeft: '113px' }}>:</span></strong> Delhi</p>
+        <p><strong>Manglik  <span style={{ marginLeft: '135px' }}>:</span></strong> No</p>
+        <p><strong>Caste/Gotra  <span style={{ marginLeft: '105px' }}>:</span></strong> Jain digamber/Garg</p>
+        <p><strong>Height <span style={{ marginLeft: '145px' }}>:</span></strong> 5 feet 10 inch</p>
+        <p><strong>Complexion <span style={{ marginLeft: '105px' }}>:</span></strong> Fair</p>
+        <p><strong>Marital Status <span style={{ marginLeft: '90px' }}>:</span></strong> Never Married</p>
+        <p><strong>Education  <span style={{ marginLeft: '118px' }}>:</span></strong> B.com from Delhi</p>
+        <p><strong>Occupation <span style={{ marginLeft: '105px' }}>:</span></strong> Business</p>
+        <p><strong>Income/Salary  <span style={{ marginLeft: '85px' }}>:</span></strong> 12 Lac</p>
+        <p><strong>Job/Business Address <span style={{ marginLeft: '20px' }}>:</span></strong> Delhi Ncr</p>
+      </div>
+
+      <div className="family-details">
+        <h2>FAMILY DETAILS</h2>
+        <p><strong>Grandfather  <span style={{ marginLeft: '105px' }}>:</span></strong> Rakesh Jain</p>
+        <p><strong>Grandmother  <span style={{ marginLeft: '95px' }}>:</span></strong> Businessman</p>
+        <p><strong>Father Name  <span style={{ marginLeft: '100px' }}>:</span></strong> Rani Jain</p>
+        <p><strong>Father Occupation  <span style={{ marginLeft: '55px' }}>:</span></strong> Housewife</p>
+        <p><strong>Mother Name  <span style={{ marginLeft: '95px' }}>:</span></strong> no</p>
+        <p><strong>Mother Occupation  <span style={{ marginLeft: '50px' }}>:</span></strong> no</p>
+        <p><strong>Sister <span style={{ marginLeft: '150px' }}>:</span></strong> XXXXXXXXX</p>
+        <p><strong>Brother  <span style={{ marginLeft: '135px' }}>:</span></strong> YYYYYYY</p>
+        <p><strong>Tauji/Chachaji <span style={{ marginLeft: '85px' }}>:</span></strong> XXXXXX</p>
+        <p><strong>Budget/Status  <span style={{ marginLeft: '85px' }}>:</span></strong> B+</p>
+      </div>
+
+      <div className="maternal-details">
+        <h2>MATERNAL DETAILS</h2>
+        <p><strong>Nana Ji  <span style={{ marginLeft: '132px' }}>:</span></strong> Nxxxxxxx</p>
+        <p><strong>Mama Ji  <span style={{ marginLeft: '125px' }}>:</span></strong> Nxxxxxxx</p>
+        <p><strong>Address  <span style={{ marginLeft: '125px' }}>:</span></strong> D-xxxxxx Street x Delhi</p>
+      </div>
+
+      <div className="contact-details">
+        <h2>CONTACT DETAILS</h2>
+        <p><strong>Residence Add. <span style={{ marginLeft: '70px' }}>:</span></strong> D-XXX Street x Delhi</p>
+        <p><strong>Contact No.  <span style={{ marginLeft: '98px' }}>:</span></strong> xxxxxxxx</p>
+        <p><strong>Whatsapp No. <span style={{ marginLeft: '80px' }}>:</span></strong> xxxxxxxx</p>
+        <p><strong>Email  <span style={{ marginLeft: '145px' }}>:</span></strong> xxxxxxxx@gmail.com</p>
+      </div>
+
+      <div className="preference-req">
+        <h2>PREFERENCE / REQ.</h2>
+        <p><strong>Qualification  <span style={{ marginLeft: '88px' }}>:</span></strong> Graduation</p>
+        <p><strong>State/City <span style={{ marginLeft: '110px' }}>:</span></strong> Delhi</p>
+        <p><strong>Kundli Match <span style={{ marginLeft: '83px' }}>:</span></strong> Yes</p>
+        <p><strong>Other  <span style={{ marginLeft: '140px' }}>:</span></strong> No</p>
+      </div>
+      </div>
+      <div className='right'> 
+        <div className='header'>
+          <h1><strong><i> BIO-DATA</i></strong></h1>
+        </div>
+      <div className="images">
+        <img src="https://images.statusfacebook.com/profile_pictures/beautiful_indian_girls/indian_girls_118.jpg" alt="Profile 1" className="profile-image" />
+        <img src="https://wallpapercave.com/wp/wp5714321.jpg" alt="Profile 2" className="profile-image" />
+      </div>
+      <div className='biodata'>
+      <h4><strong>बायोडाटा बनवाने के लिए संपर्क करें</strong></h4>
+        <h4><strong>(Jain Graphics Solution)</strong></h4>
+        
+        <h3><strong>M.8377891517</strong></h3>
+      </div>
+      </div>
     </div>
+   
+
+    </>
   );
 }
 
